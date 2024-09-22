0e99b9fa84086af8123e43c044b94bcc5b25857d
coder <coderycs@163.com>
2024-09-22 09:51:24 +0800

    feat: 桌面端打包完成

--- quasar.config.js
+++ quasar.config.js
@@ -8,9 +8,7 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
-
-const { configure } = require('quasar/wrappers');
-
+const { configure } = require("quasar/wrappers");
 
 module.exports = configure(function (/* ctx */) {
   return {
@@ -20,15 +18,10 @@ module.exports = configure(function (/* ctx */) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: [
-      
-      
-    ],
+    boot: [],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
-    css: [
-      'app.scss'
-    ],
+    css: ["app.scss"],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -40,18 +33,18 @@ module.exports = configure(function (/* ctx */) {
       // 'line-awesome',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
-      'roboto-font', // optional, you are not bound to it
-      'material-icons', // optional, you are not bound to it
+      "roboto-font", // optional, you are not bound to it
+      "material-icons", // optional, you are not bound to it
     ],
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
       target: {
-        browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
-        node: 'node20'
+        browser: ["es2019", "edge88", "firefox78", "chrome87", "safari13.1"],
+        node: "node20",
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: "hash", // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
@@ -70,7 +63,6 @@ module.exports = configure(function (/* ctx */) {
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
 
-      
       // vitePlugins: [
       //   [ 'package-name', { ..pluginOptions.. }, { server: true, client: true } ]
       // ]
@@ -79,7 +71,7 @@ module.exports = configure(function (/* ctx */) {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
       // https: true
-      open: true // opens browser window automatically
+      open: true, // opens browser window automatically
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
@@ -97,7 +89,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ["LocalStorage"],
     },
 
     // animations: 'all', // --- includes all animations
@@ -119,7 +111,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/developing-ssr/configuring-ssr
     ssr: {
       // ssrPwaHtmlFilename: 'offline.html', // do NOT use index.html as name!
-                                          // will mess up SSR
+      // will mess up SSR
 
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
@@ -130,19 +122,19 @@ module.exports = configure(function (/* ctx */) {
       // manualPostHydrationTrigger: true,
 
       prodPort: 3000, // The default port that the production server should use
-                      // (gets superseded if process.env.PORT is specified at runtime)
+      // (gets superseded if process.env.PORT is specified at runtime)
 
       middlewares: [
-        'render' // keep this as last one
-      ]
+        "render", // keep this as last one
+      ],
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/developing-pwa/configuring-pwa
     pwa: {
-      workboxMode: 'generateSW', // or 'injectManifest'
+      workboxMode: "generateSW", // or 'injectManifest'
       injectPwaMetaTags: true,
-      swFilename: 'sw.js',
-      manifestFilename: 'manifest.json',
+      swFilename: "sw.js",
+      manifestFilename: "manifest.json",
       useCredentialsForManifestTag: false,
       // useFilenameHashes: true,
       // extendGenerateSWOptions (cfg) {}
@@ -158,7 +150,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-capacitor-apps/configuring-capacitor
     capacitor: {
-      hideSplashscreen: true
+      hideSplashscreen: true,
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/configuring-electron
@@ -169,36 +161,33 @@ module.exports = configure(function (/* ctx */) {
       // specify the debugging port to use for the Electron app when running in development mode
       inspectPort: 5858,
 
-      bundler: 'packager', // 'packager' or 'builder'
+      bundler: "packager", // 'packager' or 'builder'
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Windows only
         // win32metadata: { ... }
+        plateform: "win32",
       },
 
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'quasar-counter'
-      }
+        appId: "quasar-counter",
+      },
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/developing-browser-extensions/configuring-bex
     bex: {
-      contentScripts: [
-        'my-content-script'
-      ],
+      contentScripts: ["my-content-script"],
 
       // extendBexScriptsConf (esbuildConf) {}
       // extendBexManifestJson (json) {}
-    }
-  }
+    },
+  };
 });
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,110 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page
+    class="flex flex-center text-white"
+    v-touch-pan.prevent.mouse="handlePan"
+  >
+    <div class="row">
+      <q-input
+        v-model="data.name"
+        input-class="text-center text-h5 text-white"
+        color="teal"
+        filled
+        placeholder="Counter"
+      />
+    </div>
+    <div class="row full-width items-center">
+      <div class="col text-center">
+        <q-btn
+          v-touch-repeat:300:300:300:300:50.mouse="decreaseCounter"
+          size="xl"
+          round
+          color="primary"
+          icon="remove"
+          @click="decreaseCounter"
+        />
+      </div>
+      <div class="col text-center text-h2">{{ data.counter }}</div>
+      <div class="col text-center">
+        <q-btn
+          v-touch-repeat:300:300:300:300:50.mouse="increaseCounter"
+          size="xl"
+          round
+          color="primary"
+          icon="add"
+          @click="increaseCounter"
+        />
+      </div>
+    </div>
+    <div class="row">
+      <q-btn
+        size="xl"
+        round
+        color="primary"
+        icon="restart_alt"
+        @click="resetCounter"
+      />
+    </div>
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
+
+/**
+ * import
+ */
+import { reactive, watch } from "vue";
+import { useQuasar } from "quasar";
+
+/**
+ * quasar
+ */
+const $q = useQuasar();
+
+/**
+ * data
+ */
+const data = reactive({
+  counter: 0,
+  name: "",
+});
+
+const savedData = $q.localStorage.getItem("data");
+if (savedData) Object.assign(data, savedData);
+
+watch(data, (value) => {
+  $q.localStorage.set("data", value);
+});
+
+/**
+ * counter methods
+ */
+const increaseCounter = () => {
+  data.counter++;
+};
+
+const decreaseCounter = () => {
+  if (data.counter > 0) {
+    data.counter--;
+  }
+};
+
+const resetCounter = () => {
+  data.counter = 0;
+  data.name = "";
+};
+
+const handlePan = (e) => {
+  if (e.delta.y < 0) increaseCounter();
+  else decreaseCounter();
+};
 </script>
+
+<style scoped>
+.q-page {
+  max-width: 700px;
+  margin: 0 auto;
+}
+</style>
